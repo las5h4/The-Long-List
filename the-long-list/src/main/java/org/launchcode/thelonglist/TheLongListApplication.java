970229df0d7ac8ef39846ffe08f6f24926850f73
las5h4 <loganasmalley@gmail.com>
2020-04-15 14:14:09 -0500

    created gradle project

---
+++ the-long-list/src/main/java/org/launchcode/thelonglist/TheLongListApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.thelonglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLongListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLongListApplication.class, args);
+	}
+
+}
---
+++ the-long-list/src/test/java/org/launchcode/thelonglist/TheLongListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.thelonglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLongListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
