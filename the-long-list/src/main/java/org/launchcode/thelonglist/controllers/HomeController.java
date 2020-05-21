acd79df5204fce317e18e1981d55041f33c25331
las5h4 <loganasmalley@gmail.com>
2020-05-21 17:03:08 -0500

    Figured out how to call user info from the session

--- the-long-list/src/main/java/org/launchcode/thelonglist/controllers/HomeController.java
+++ the-long-list/src/main/java/org/launchcode/thelonglist/controllers/HomeController.java
@@ -1,13 +1,27 @@
 package org.launchcode.thelonglist.controllers;
 
+import org.launchcode.thelonglist.data.UserRepository;
+import org.launchcode.thelonglist.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping
-    public String index() {
+    public String index(HttpSession session, Model model) {
+        Object userId = session.getAttribute("user");
+        Optional<User> result = userRepository.findById((Integer) userId);
+        User user = result.get();
+        model.addAttribute("username", user.getUsername());
         return "index";
     }
 
